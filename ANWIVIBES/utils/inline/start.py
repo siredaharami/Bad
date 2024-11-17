d911c97e3fd43130ee6a1d52d8986e85d44e9cf0
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-18 01:11:22 +0530

    Update start.py

--- ANWIVIBES/utils/inline/start.py
+++ ANWIVIBES/utils/inline/start.py
@@ -37,6 +37,6 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-#       [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+       [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
     ]
     return buttons
