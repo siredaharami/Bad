910980b13fc78aff1161988baa51014839e801a2
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-18 02:10:44 +0530

    Update __main__.py

--- ANWIVIBES/__main__.py
+++ ANWIVIBES/__main__.py
@@ -1,15 +1,25 @@
+#
+# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
+#
+# This file is part of < https://github.com/IamDvis/DV-VIBES > project,
+# and is released under the MIT License.
+# Please see < https://github.com/IamDvis/DV-VIBES/blob/master/LICENSE >
+#
+# All rights reserved.
 
 import asyncio
 import importlib
 
 from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from config import BANNED_USERS
-from ANWIVIBES import HELPABLE, LOGGER, app, userbot
+from ANWIVIBES import LOGGER, app, userbot
 from ANWIVIBES.core.call import ANWI
+from ANWIVIBES.misc import sudo
 from ANWIVIBES.plugins import ALL_MODULES
 from ANWIVIBES.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
 
 
 async def init():
@@ -20,15 +30,9 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("ANWIVIBES").error(
-            "No Assistant Clients Vars Defined!.. Exiting Process."
-        )
-        return
-    if not config.SPOTIFY_CLIENT_ID and not config.SPOTIFY_CLIENT_SECRET:
-        LOGGER("ANWIVIBES").warning(
-            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
-        )
-
+        LOGGER(__name__).error("✦ Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
     try:
         users = await get_gbanned()
         for user_id in users:
@@ -36,26 +40,32 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except Exception:
+    except:
         pass
-
     await app.start()
-
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(all_module)
-
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("ANWIVIBES.plugins").info("Successfully Imported All Modules ")
-
+        importlib.import_module("ANWIVIBES.plugins" + all_module)
+    LOGGER("ANWIVIBES.plugins").info("✦ Successfully Imported Modules...💞")
     await userbot.start()
     await ANWI.start()
+    try:
+        await ANWI.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("ANWIVIBES").error(
+            "✦ Please turn on the videochat of your log group\channel.\n\n✦ Stopping Bot...💣"
+        )
+        exit()
+    except:
+        pass
     await ANWI.decorators()
-    LOGGER("ANWIVIBES").info("ANWIVIBES STARTED SUCCESSFULLY 🕊️")
+    LOGGER("ANWIVIBES").info(
+        "✦ Created By ➥ The Dvis...🐝"
+    )
     await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("ANWIVIBES").info("❖ Stopping ANWI VIBES Bot...💌")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
-    LOGGER("ANWIVIBES").info("Stopping ANWIVIBES! GoodBye")
+    asyncio.get_event_loop().run_until_complete(init())
