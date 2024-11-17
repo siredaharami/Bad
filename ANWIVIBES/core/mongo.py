e14e43d5a43ec1d9df6049951f270357d4989699
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-18 00:07:34 +0530

    Update mongo.py

--- ANWIVIBES/core/mongo.py
+++ ANWIVIBES/core/mongo.py
@@ -14,7 +14,7 @@ import config
 from ..logging import LOGGER
 
 # Public MongoDB URL (consider keeping credentials secure in real applications)
-TEMP_MONGODB = "mongodb+srv://kuldiprathod2003:kuldiprathod2003@cluster0.wxqpikp.mongodb.net/?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"
 
 try:
     # Check if a custom MongoDB URI is provided in the config
