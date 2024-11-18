ea14bb320590bd4b912bd120b119b0ed588b9a86
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-18 23:30:50 +0530

    Update database.py

--- ANWIVIBES/utils/database.py
+++ ANWIVIBES/utils/database.py
@@ -709,6 +709,37 @@ async def remove_banned_user(user_id: int):
         return
     return await blockeddb.delete_one({"user_id": user_id})
 
+# Private Served Chats
+
+
+async def get_private_served_chats() -> list:
+    chats_list = []
+    async for chat in privatedb.find({"chat_id": {"$lt": 0}}):
+        chats_list.append(chat)
+    return chats_list
+
+
+async def is_served_private_chat(chat_id: int) -> bool:
+    chat = await privatedb.find_one({"chat_id": chat_id})
+    if not chat:
+        return False
+    return True
+
+
+async def add_private_chat(chat_id: int):
+    is_served = await is_served_private_chat(chat_id)
+    if is_served:
+        return
+    return await privatedb.insert_one({"chat_id": chat_id})
+
+
+async def remove_private_chat(chat_id: int):
+    is_served = await is_served_private_chat(chat_id)
+    if not is_served:
+        return
+    return await privatedb.delete_one({"chat_id": chat_id})
+    
+
 # Video Limit
 async def is_video_allowed(chat_idd) -> str:
     chat_id = 123456
