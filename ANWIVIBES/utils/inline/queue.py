6b619905c6a2522c753d0a170c9985efdfcc138a
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-18 09:52:46 +0530

    Update queue.py

--- ANWIVIBES/utils/inline/queue.py
+++ ANWIVIBES/utils/inline/queue.py
@@ -76,7 +76,7 @@ def aq_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="𓊕", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="။", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
