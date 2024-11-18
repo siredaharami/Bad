9e4f42df2703af0415f163cd5c29bf8de9b55d74
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-19 01:00:30 +0530

    Update database.py

--- ANWIVIBES/utils/database.py
+++ ANWIVIBES/utils/database.py
@@ -86,7 +86,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from BADMUSIC.core.userbot import assistants
+    from ANWIVIBES.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -100,7 +100,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from BADMUSIC.core.userbot import assistants
+    from ANWIVIBES.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -155,7 +155,7 @@ async def get_afk_users() -> list:
   
 
 async def set_calls_assistant(chat_id):
-    from BADMUSIC.core.userbot import assistants
+    from ANWIVIBES.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -168,7 +168,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from BADMUSIC.core.userbot import assistants
+    from ANWIVIBES.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
