8c2beb7cbee38c1d4feeea1fc22ddc20cc08a7cd
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-19 17:24:30 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -38,9 +38,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600000))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 
 #❖ Get this value from @MissRose_bot on Telegram by /id
-OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "7009601543").split())
-)  # Input type must be interger
+OWNER_ID = getenv("OWNER_ID", "7009601543")
 
 #❖  Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
