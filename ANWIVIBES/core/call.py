1cf7f97b3725f3e9582ba23532c8997148809131
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-19 16:57:54 +0530

    Update call.py

--- ANWIVIBES/core/call.py
+++ ANWIVIBES/core/call.py
@@ -19,7 +19,6 @@ from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
-    TelegramServerError,
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
