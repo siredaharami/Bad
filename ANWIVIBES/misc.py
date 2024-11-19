8df44dfbaa2c6a5d7b5a6f6adc7d8b6d1f69ee59
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-19 22:34:09 +0530

    Update misc.py

--- ANWIVIBES/misc.py
+++ ANWIVIBES/misc.py
@@ -44,7 +44,8 @@ def dbb():
 
 async def sudo():
     global SUDOERS
-    SUDOERS.add(config.OWNER_ID)
+    for owner_id in config.OWNER_ID:
+    SUDOERS.add(owner_id)
     sudoersdb = mongodb.sudoers
     sudoers = await sudoersdb.find_one({"sudo": "sudo"})
     sudoers = [] if not sudoers else sudoers["sudoers"]
