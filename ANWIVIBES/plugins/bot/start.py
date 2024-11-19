e2c01ece4ae4490f1d84758c24cd73b8c6e09311
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-11-19 17:17:17 +0530

    Update start.py

--- ANWIVIBES/plugins/bot/start.py
+++ ANWIVIBES/plugins/bot/start.py
@@ -151,6 +151,11 @@ async def start_pm(client, message: Message, _):
                     caption=_["start_2"].format(message.from_user.mention, app.mention),
                     reply_markup=InlineKeyboardMarkup(out),
                 )
+        else:
+            await message.reply_photo(
+                caption=_["start_2"].format(message.from_user.mention, app.mention),
+                reply_markup=InlineKeyboardMarkup(out),
+            )
         if await is_on_off(config.LOG):
             sender_id = message.from_user.id
             sender_name = message.from_user.first_name
